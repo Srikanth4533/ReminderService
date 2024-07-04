7c1994750aba4f1a9f1c7d39559988ef5f1a6d19
Srikanth4533 <kondakallasrikanth991@gmail.com>
2024-07-04 09:38:29 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 
+const cron = require("node-cron")
+
 const  { PORT } = require("./config/serverConfig")
 const  { sendBasicEmail } = require("./services/email-service")
 
@@ -13,13 +15,16 @@ const setupAndStartServer = async () => {
     app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`)
 
-        sendBasicEmail(
-            'support@sri.com',
-            'srikanthsri4533@gmail.com',
+        
+        cron.schedule('* * * * *', () => {
+            sendBasicEmail(
+            'Srikanth support@sri.com',
+            'paravindgoud77@gmail.com',
             'From Srikanth',
             'Hello Namaste anna! baagunnavey'
         )
-        console.log("email sent")
+        console.log("hi")
+        })
     })
 }
 
